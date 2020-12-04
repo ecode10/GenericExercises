c230b442c6b7661b0b5e6174d2e781de744b40f6
Mauricio Junior <mauricio.junior@gmail.com>
2020-12-04 16:33:12 -0600

    add métodos genéricos
    add métodos genéricos

---
+++ GenericsCSharp/GenericsCSharp/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace GenericsCSharp
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+        public Animal(string nome = "Sem nome")
+        {
+            Nome = nome;
+        }
+
+        public static void GetSum<T>(ref T num1, ref T num2)
+        {
+            double n1 = Convert.ToDouble(num1);
+            double n2 = Convert.ToDouble(num2);
+
+            Console.WriteLine($"{n1} + {n2} = {n1 + n2}");
+        }
+    }
+}
---
+++ GenericsCSharp/GenericsCSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<Animal>()
+            {
+                new Animal
+                {
+                    Nome = "Dog"
+                },
+
+                new Animal
+                {
+                    Nome = "Cat"
+                },
+
+                new Animal
+                {
+                    Nome = "Lion"
+                },
+
+                new Animal
+                {
+                    Nome = "Elephant"
+                }
+            };
+
+            Console.WriteLine($"Total de animais: {animals.Count}");
+
+            var selectAnimais = from animal in animals
+                                //where animal.Nome.Contains("a")
+                                select animal;
+
+            foreach (var animal in selectAnimais)
+            {
+                Console.WriteLine($"Nome dos animais: {animal.Nome}");
+            }
+
+            //método genérico
+            int x = 5, y = 5;
+            Animal.GetSum(ref x, ref y);
+
+            string a = "6", b = "10";
+            Animal.GetSum(ref a, ref b);
+
+            Console.ReadLine();
+        }
+    }
+}
